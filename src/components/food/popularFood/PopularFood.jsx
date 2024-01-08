410ef297d01d1415a86cf08b8868e5f1ba029711
sabbir-62 <sabbir.hossain.se62@gmail.com>
2024-01-08 15:23:48 +0600

    update

--- src/components/food/popularFood/PopularFood.jsx
+++ src/components/food/popularFood/PopularFood.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./popularFood.scss";
-import { useNavigate } from "react-router-dom";
+
 
 const PopularFood = () => {
   const [data, setData] = useState([]);
