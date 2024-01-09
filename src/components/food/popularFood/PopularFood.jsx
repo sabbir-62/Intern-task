c5aadbff9a5ae00bd63c541cad1bfd576e68de56
sabbir-62 <sabbir.hossain.se62@gmail.com>
2024-01-09 15:03:26 +0600

    update

--- src/components/food/popularFood/PopularFood.jsx
+++ src/components/food/popularFood/PopularFood.jsx
@@ -12,7 +12,7 @@ const PopularFood = () => {
     //read all data
     const readAllData = async () => {
       try {
-        const apiUrl = "http://www.api.technicaltest.quadtheoryltd.com/api/Item?page=1&pageSize=10";
+        const apiUrl ="http://www.api.technicaltest.quadtheoryltd.com/api/Item?page=1&pageSize=10";
         const response = await axios.get( apiUrl);
         setData(response.data.Items);
       } catch (error) {
