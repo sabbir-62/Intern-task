063af8666f7f132dc4c4dd016b673b621b893179
sabbir-62 <sabbir.hossain.se62@gmail.com>
2024-01-09 18:06:33 +0600

    update

--- src/components/food/popularFood/PopularFood.jsx
+++ src/components/food/popularFood/PopularFood.jsx
@@ -12,7 +12,7 @@ const PopularFood = () => {
     //read all data
     const readAllData = async () => {
       try {
-        const apiUrl ="https://www.api.technicaltest.quadtheoryltd.com/api/Item?page=1&pageSize=10";
+        const apiUrl ="/.netlify/functions/proxy";
         const response = await axios.get( apiUrl);
         setData(response.data.Items);
       } catch (error) {
--- src/components/food/popularFood/RecomendedFood.jsx
+++ src/components/food/popularFood/RecomendedFood.jsx
@@ -16,7 +16,7 @@ const RecommendedFood = () => {
     //read all data
     const readAllData = async () => {
       try {
-        const apiUrl ="https://www.api.technicaltest.quadtheoryltd.com/api/Item?page=1&pageSize=10";
+        const apiUrl ="/.netlify/functions/proxy";
         const response = await axios.get(apiUrl);
         setData(response.data.Items);
       } catch (error) {
