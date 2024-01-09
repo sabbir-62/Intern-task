f1f9f1d727cad8d51b8034cf0987d43d0cc2a0da
sabbir-62 <sabbir.hossain.se62@gmail.com>
2024-01-09 12:12:52 +0600

    update

--- src/components/food/popularFood/RecomendedFood.jsx
+++ src/components/food/popularFood/RecomendedFood.jsx
@@ -16,8 +16,8 @@ const RecommendedFood = () => {
     //read all data
     const readAllData = async () => {
       try {
-        const apiUrl =
-          "http://www.api.technicaltest.quadtheoryltd.com/api/Item?page=1&pageSize=10";
+        const apiUrl = "https://cors-anywhere.herokuapp.com/http://www.api.technicaltest.quadtheoryltd.com/api/Item?page=1&pageSize=10";
+
         const response = await axios.get(apiUrl);
         setData(response.data.Items);
       } catch (error) {
